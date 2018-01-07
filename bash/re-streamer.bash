4b59e24f8f947aa458dcb08f94c7866e75060293
Batuhan Osman Taşkaya <batuhanosmantaskaya@gmail.com>
2018-01-07 18:01:21 +0300

    [re-streamer.bash] Indentetion Fixment
    - Files: re-streamer.bash
    - Changes: Tabs re-writed as spaces

--- bash/re-streamer.bash
+++ bash/re-streamer.bash
@@ -44,61 +44,61 @@ callthepolice=`ls $DEST$topkek$FORMAT 2> /dev/null | wc -l`
 callthehouse=`ls $DEST$topkek$FORMAT2 2> /dev/null | wc -l`
 
 case $1 in
-	start )
-		if [ -z $2 ]; then
-			printf "\nNo link is specified.\n\nUsage: $0 start <link> <optional-quality>"
-			exit
-		fi
+    start )
+        if [ -z $2 ]; then
+            printf "\nNo link is specified.\n\nUsage: $0 start <link> <optional-quality>"
+            exit
+        fi
 
-		if [ -z $3  ]; then
-			quality=best
-		else
-			quality=$3
-		fi
+        if [ -z $3  ]; then
+            quality=best
+        else
+            quality=$3
+        fi
 
-		if [ ! -z $(pgrep streamlink) ] || [ ! -z $(pgrep ffmpeg) ]; then
-			printf "\nNecessary services are already running. Exiting...\n\n"
-			exit
-		fi
+        if [ ! -z $(pgrep streamlink) ] || [ ! -z $(pgrep ffmpeg) ]; then
+            printf "\nNecessary services are already running. Exiting...\n\n"
+            exit
+        fi
 
-		if find_screen "streamers" >/dev/null; then
-			screen -S streamers -X stuff "^Mexit^M"
-			sleep 1
-		fi
+        if find_screen "streamers" >/dev/null; then
+            screen -S streamers -X stuff "^Mexit^M"
+            sleep 1
+        fi
 
-		screen -dmS streamers bash -c 'echo Startup!;  exec bash'
-		sleep 1
-		screen -S streamers -X stuff $"streamlink -O $2 $quality | ffmpeg -v verbose -i pipe:0 -c:v libx264 -c:a aac -ac 1 -crf 23 -profile:v baseline -maxrate 400k -bufsize 1835k -pix_fmt yuv420p -flags -global_header -hls_time 10 -hls_list_size 6 -hls_wrap 10 -start_number 1 $DEST$streamName.m3u8\n"
-		printf "\nStartup initiated.\n\n"
-	;;
-	stop )
+        screen -dmS streamers bash -c 'echo Startup!;  exec bash'
+        sleep 1
+        screen -S streamers -X stuff $"streamlink -O $2 $quality | ffmpeg -v verbose -i pipe:0 -c:v libx264 -c:a aac -ac 1 -crf 23 -profile:v baseline -maxrate 400k -bufsize 1835k -pix_fmt yuv420p -flags -global_header -hls_time 10 -hls_list_size 6 -hls_wrap 10 -start_number 1 $DEST$streamName.m3u8\n"
+        printf "\nStartup initiated.\n\n"
+    ;;
+    stop )
         if [ ! -z $(pgrep streamlink) ] && [ ! -z $(pgrep ffmpeg) ]; then
             if find_screen "streamers" >/dev/null; then
-             	if [ ! -z $(pgrep ffmpeg) ]; then
-					kill $(pgrep ffmpeg)
-					printf "\nWaiting for graceful quit for 10 seconds..."
-					sleep 10
-				fi
+                 if [ ! -z $(pgrep ffmpeg) ]; then
+                    kill $(pgrep ffmpeg)
+                    printf "\nWaiting for graceful quit for 10 seconds..."
+                    sleep 10
+                fi
 
-				if [ ! -z $(pgrep streamlink) ]; then
-					kill $(pgrep streamlink)
-				fi
+                if [ ! -z $(pgrep streamlink) ]; then
+                    kill $(pgrep streamlink)
+                fi
 
-				screen -S streamers -X stuff "^Mexit^M"
+                screen -S streamers -X stuff "^Mexit^M"
 
-				if [ $callthepolice != "0" ]; then
-					rm $DEST$topkek$FORMAT
-				fi
+                if [ $callthepolice != "0" ]; then
+                    rm $DEST$topkek$FORMAT
+                fi
 
-				if [ $callthehouse != "0" ]; then
-					rm $DEST$topkek$FORMAT2
-				fi
+                if [ $callthehouse != "0" ]; then
+                    rm $DEST$topkek$FORMAT2
+                fi
 
-				printf "\nStream stopped.\n\n"
+                printf "\nStream stopped.\n\n"
             fi
         fi
-	;;
-	* )
-		exit
-	;;
+    ;;
+    * )
+        exit
+    ;;
 esac
